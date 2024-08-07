d148171062e55ab384e6b44b8893006073891a5f
sfh <sfhery@foxmail.com>
2024-08-07 15:11:44 +0800

    add new router func

--- SparkRouter/Classes/SparkRouter.swift
+++ SparkRouter/Classes/SparkRouter.swift
@@ -9,7 +9,26 @@ import CTMediator
 
 public extension CTMediator {
     
+    /// 获取公共WebVC
+    /// - Parameters:
+    ///   - param: 参数集合
+    ///   - callback: 回调
+    /// - Returns: 目标vc
+    @objc func getBookShopWeb(_ param: [AnyHashable: Any], callback: (() -> ())? = nil) -> UIViewController? {
+        var params = param
+        params["call"] = callback
+        params[kCTMediatorParamsKeySwiftTargetModuleName] = "SparkBase"
+        
+        guard let vc = self.performTarget("BSWeb", action: "toBookShopWebVC", params: params, shouldCacheTarget: false) as? UIViewController else { return nil }
+        
+        return vc
+    }
+    
     /// 获取视频播放器VC
+    /// - Parameters:
+    ///   - param: 参数集合
+    ///   - callback: 回调
+    /// - Returns: 目标vc
     @objc func getLFVideoPlayer(param: [String: Any], callback: ((String, String) -> Void)? = nil) -> UIViewController? {
         var params = param as [AnyHashable: Any]
         params["popblock"] = callback
@@ -20,7 +39,10 @@ public extension CTMediator {
         return vc
     }
     
+    
     /// 获取原生做题VC
+    /// - Parameter param: 参数集合
+    /// - Returns: 目标vc
     @objc func getTestPaperNative(param: [String: Any]) -> UIViewController? {
         var params = param as [AnyHashable: Any]
         params[kCTMediatorParamsKeySwiftTargetModuleName] = "SparkBase"
